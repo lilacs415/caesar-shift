ba44f48178a5f0ac9e825eb017da0c97bf85a46a
Lila Chen <lilacs@gmail.com>
2022-05-31 15:30:29 -0700

    everything except decode

--- caesarshift.py
+++ caesarshift.py
@@ -1,30 +1,41 @@
 import pytest
-# num_to_val = lambda string: [ord(char) - 96 for char in string]
-def num_to_val(string):
-    if string.isalpha():
-        return [ord(char) - 96 for char in string]
-# val_to_num = lambda letter_vals: ''.join([chr(val + 96) for val in letter_vals if val != 32])
 
-def val_to_num(letter_vals):
-    return ''.join([chr(val + 96) for val in letter_vals if val != 32])
-    
+
+val_to_let = lambda letter_vals: ''.join([chr(val) for val in letter_vals])
+
+
 def caesar_shift(words, shift):
     """
     where words is a list of strings of text to shift
     """
-    shifted = []
+    if shift > 25:
+        return 'Please enter a shift number between 1 and 25, inclusive.'
+    numbers = []
     for word in words:
-        nums = num_to_val(word)
-        intermediate = [num + shift for num in nums]
-        shifted.append(val_to_num(intermediate))
-    return shifted
+        if word.isalpha():
+            for letter in word:
+                if letter.islower():
+                    if ord(letter) + shift > 122:
+                        numbers.append(98 + (shift - (122 - ord(letter) + shift)))
+                    else:
+                        numbers.append(ord(letter) + shift)
+                else:
+                    if ord(letter) + shift > 90:
+                        numbers.append(66 + (shift - (90 - ord(letter) + shift)))
+                    else:
+                        numbers.append(ord(letter) + shift)
+            numbers.append(32)
+    return val_to_let(numbers).strip() if numbers else "Please input only words into the shifter."
 
 
 def test_answer():
-    assert caesar_shift(['hello', 'world'], 2) == ['jgnnq', 'yqtnf']
-    # assert caesar_shift(['xyz'], 2) == ['zab']
-    assert caesar_shift(['ABC'], 2) == ['CDE']
-    assert caesar_shift(['AbCdEf'], 2) == ['CdEfGh']
+    assert caesar_shift(['hello', 'world'], 2) == 'jgnnq yqtnf'
+    assert caesar_shift(['xyz'], 2) == 'zab'
+    assert caesar_shift(['XYZ'], 2) == 'ZAB'
+    assert caesar_shift(['ABC'], 2) == 'CDE'
+    assert caesar_shift(['AbCdEf'], 2) == 'CdEfGh'
+    assert caesar_shift(['123'], 2) == "Please input only words into the shifter."
+
 
 if __name__ == "__main__":
     def repl():
@@ -33,17 +44,16 @@ if __name__ == "__main__":
         """
         print('To use this tool, please enter an "encode" or "decode" command, then your text, then if applicable the shift number.')
         user_input = input('input: ')
-        while user_input != 'exit.':
+        while user_input != 'exit':
             raw_inputs = user_input.split()
             inputs = [raw_inputs[0], raw_inputs[1:-1], raw_inputs[-1]]
             if inputs[0] == 'encode':
-                if inputs[-1].isnumeric():
-                    print('output: ', ' '.join(caesar_shift(inputs[1], int(inputs[2]))))
+                if inputs[-1].isnumeric() and len(inputs) == 3:
+                    print('output: ', caesar_shift(inputs[1], int(inputs[2])))
                 else:
                     print('Error: please provide a shift number after the text you would like to encode.')
             elif inputs[0] == 'decode':
-                # TODO: make this decode
-                print("output: ", caesar_shift(inputs[0], int(inputs[1])))
+                print("output: ", caesar_shift(inputs[1], 25 - int(inputs[2])))
             else:
                 print('Error: please precede your text with an "encode" or "decode" command.')
             user_input = input('input: ')
